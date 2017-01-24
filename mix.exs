e75d071cbdc43ff425fdda19b6c8876e777d0a0d
Andriy Borodiychuk <ab@markusweb.com>
2017-01-24 21:08:46 +0100

    Inchi CI

--- mix.exs
+++ mix.exs
@@ -29,8 +29,9 @@ defmodule UeberauthFoursquare.Mixfile do
       {:oauth2,    "~> 0.8"},
       {:ueberauth, "~> 0.4"},
       {:credo,     "~> 0.5",   only: [:dev, :test]},
-      {:earmark,   "~> 0.2",   only: :dev},
-      {:ex_doc,    ">= 0.0.0", only: :dev}
+      {:earmark,   "~> 1.1",   only: :dev},
+      {:ex_doc,    ">= 0.0.0", only: :dev},
+      {:inch_ex,               only: :docs}
     ]
   end
 
